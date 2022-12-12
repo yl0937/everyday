4a3d1a18b67040b494595fe0caaa5fef713945f8
yl0937 <60501504+yl0937@users.noreply.github.com>
2022-12-12 16:04:44 +0900

    set1

--- config/database.js
+++ config/database.js
@@ -3,11 +3,12 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '43.200.244.4',
+    // host: '43.200.244.4',
+    host: 'localhost',
     user: 'master',
     port: '3306',
-    password: '@master123',
-    database: 'capstone'
+    password: 'master',
+    database: 'ott'
 });
 
 module.exports = {
